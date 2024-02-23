a4060ebc0a943ddd9011bbf832bfb41fcbd79b3c
unknown <icyveins@hotmail.com>
2024-02-23 19:02:18 +0800

    add preprocessor float relative thresholds
    shows that the AVX single precision one is too poor for 1e8 lengths; it
    is still failing at 10% levels

--- test/basic.cpp
+++ test/basic.cpp
@@ -7,9 +7,17 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#ifdef __AVX__
+#define SINGLE_REL_THRESHOLD_SHORT 1e-3
+#define SINGLE_REL_THRESHOLD_LONG 0.5
+#else
+#define SINGLE_REL_THRESHOLD_SHORT 1e-6
+#define SINGLE_REL_THRESHOLD_LONG 1e-5
+#endif
+
 
 template <typename T>
-void test_basic(size_t len, double freq, double phase, T threshold)
+void test_basic(size_t len, double freq, double phase, double threshold)
 {
     // Create some vectors
     std::vector<std::complex<T>> data(len);
@@ -26,15 +34,30 @@ void test_basic(size_t len, double freq, double phase, T threshold)
     ffs::shiftVector<T>(data, freq, phase);
 
     // Check
+    bool printed = false;
     for (size_t i = 0; i < data.size(); i++)
     {
-        std::complex<T> correct = data2[i] * std::complex<T>(
+        // we always cast to double first to calculate the checks
+        std::complex<double> correct = static_cast<std::complex<double>>(data2[i]) * std::complex<double>(
             std::cos(2 * M_PI * freq * i + phase),
             std::sin(2 * M_PI * freq * i + phase)
         );
 
-        REQUIRE_THAT(data[i].real(), Catch::Matchers::WithinRel(correct.real(), threshold));
-        REQUIRE_THAT(data[i].imag(), Catch::Matchers::WithinRel(correct.imag(), threshold));
+        double dreal = static_cast<double>(data[i].real());
+        double dimag = static_cast<double>(data[i].imag());
+
+        if (std::abs(dreal - correct.real()) / correct.real() > threshold && !printed)
+        {
+            printf("real: %f vs %f\n", dreal, correct.real());
+            printed = true; // just show once
+        }
+
+        REQUIRE_THAT(
+            dreal, 
+            Catch::Matchers::WithinRel(correct.real(), threshold));
+        REQUIRE_THAT(
+            dimag,
+            Catch::Matchers::WithinRel(correct.imag(), threshold));
     }
 }
 
@@ -60,18 +83,18 @@ TEST_CASE("basic double", "[basic],[double]")
 
 TEST_CASE("basic float", "[basic],[float]")
 {
-    SECTION("len 1e5, freq 1e-9, phase 0.1, relative threshold 1e-8"){
-        test_basic<float>(100000, 1e-9, 0.1, 1e-9);
+    SECTION("len 1e5, freq 1e-9, phase 0.1"){
+        test_basic<float>(100000, 1e-9, 0.1, SINGLE_REL_THRESHOLD_SHORT);
     }
 
     // This is to check non-multiple of UNROLL lengths
-    SECTION("len 1e5-1, freq 1e-9, phase 0.1, relative threshold 1e-8"){
-        test_basic<float>(99999, 1e-9, 0.1, 1e-9);
+    SECTION("len 1e5-1, freq 1e-9, phase 0.1"){
+        test_basic<float>(99999, 1e-9, 0.1, SINGLE_REL_THRESHOLD_SHORT);
     }
     
     // For long lengths, relative threshold must be increased in order to pass
-    SECTION("len 1e8, freq 1e-9, phase 0.1, relative threshold 1e-5"){
-        test_basic<float>(100000000, 1e-9, 0.1, 1e-5);
+    SECTION("len 1e8, freq 1e-9, phase 0.1"){
+        test_basic<float>(100000000, 1e-9, 0.1, SINGLE_REL_THRESHOLD_LONG);
     }
 
 }
